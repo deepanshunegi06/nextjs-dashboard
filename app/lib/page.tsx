c4989f78999f7807c8210c0075b22ca6bd5fde2d
Deepanshu negi <deepanshunegi98@gmail.com>
2024-12-30 14:29:50 +0530

    Initial commit

---
+++ app/lib/page.tsx
@@ -0,0 +1,3 @@
+export default function page(){
+    return <h1>hi i am the lib</h1>
+}
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -9,13 +9,13 @@ export default function SideNav() {
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
         href="/"
-      >
+      > 
         <div className="w-32 text-white md:w-40">
           <AcmeLogo />
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <NavLinks />
+        <NavLinks />  
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
